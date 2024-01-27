9c94e0c29f5b6965e4557707e1d184ebe39e5120
Obai Basher <53649412+ObaiBasheer@users.noreply.github.com>
2024-01-27 13:36:38 +0300

    Add project files.

---
+++ APIHarbor/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace APIHarbor.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
